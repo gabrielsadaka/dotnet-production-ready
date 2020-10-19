7ec1f7016f6c768c71823a4db9cdd677a7d6c408
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-19 19:18:57 +1100

    Login to gcp using docker before deploy

--- infra/build-push.sh
+++ infra/build-push.sh
@@ -8,6 +8,8 @@ npm --prefix infra/build-push install infra/build-push
 
 echo "Applying changes"
 
+echo $GOOGLE_CREDENTIALS | docker login -u _json_key --password-stdin https://gcr.io
+
 pulumi stack select dev -c --non-interactive --cwd infra/build-push
 
 pulumi up --stack dev --non-interactive --yes --cwd infra/build-push
