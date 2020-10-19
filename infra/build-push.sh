9d3c920cffffa368c864cf29c560c6514899b8cd
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-19 18:43:41 +1100

    Fix pulumi command usage

--- infra/build-push.sh
+++ infra/build-push.sh
@@ -8,6 +8,6 @@ npm --prefix infra/build-push install infra/build-push
 
 echo "Applying changes"
 
-pulumi stack select dev -c --non-interactive -cwd infra/build-push
+pulumi stack select dev -c --non-interactive --cwd infra/build-push
 
-pulumi up --stack dev --non-interactive --yes -cwd infra/build-push
+pulumi up --stack dev --non-interactive --yes --cwd infra/build-push
--- infra/deploy-base.sh
+++ infra/deploy-base.sh
@@ -8,6 +8,6 @@ npm --prefix infra/base install infra/base
 
 echo "Applying changes"
 
-pulumi stack select dev -c --non-interactive -cwd infra/base
+pulumi stack select dev -c --non-interactive --cwd infra/base
 
-pulumi up --stack dev --non-interactive --yes -cwd infra/base
+pulumi up --stack dev --non-interactive --yes --cwd infra/base
