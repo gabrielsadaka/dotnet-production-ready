bfb3b95635f885f749b79d13e1f3be7c427d4fa7
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-19 11:37:26 +1100

    Wait for postgres to start before running migrations

--- db/scripts/run-migrations.sh
+++ db/scripts/run-migrations.sh
@@ -3,6 +3,14 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+RETRIES=10
+until PGPASSWORD=$PASSWORD psql -h $HOST -U $USERNAME -d $DATABASE_NAME -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 1
+done
+
+echo "Postgres ready, running migrations"
+
 # clean the database
 >&2 echo "Flyway is cleaning the database first"
 flyway -url=jdbc:postgresql://"$HOST":"$PORT"/"$DATABASE_NAME" -schemas="$SCHEMA_NAME" -user="$USERNAME" -password="$PASSWORD" clean
