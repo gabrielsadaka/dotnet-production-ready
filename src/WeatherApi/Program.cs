3d3cd0dfb5ef00ab51651a2e6cb65c8401582d46
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-21 09:00:03 +1100

    Listen to port set by Cloud Run env

--- src/WeatherApi/Program.cs
+++ src/WeatherApi/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -12,8 +13,15 @@ namespace WeatherApi
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            var url = string.Concat("http://0.0.0.0:", port);
             return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseUrls(url);
+                });
         }
     }
 }
