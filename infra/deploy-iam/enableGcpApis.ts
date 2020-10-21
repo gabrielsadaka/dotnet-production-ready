337d5b958582a7a45b02d233a53fea9a3052da2e
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-21 11:47:49 +1100

    Make iam changes depend on IAM API being enabled

--- infra/deploy-iam/enableGcpApis.ts
+++ infra/deploy-iam/enableGcpApis.ts
@@ -1,18 +1,18 @@
 import * as gcp from "@pulumi/gcp";
 
 // Enable required GCP APIs
-const enableCloudRunApi= new gcp.projects.Service("EnableCloudRunApi", {
+export const enableCloudRunApi= new gcp.projects.Service("EnableCloudRunApi", {
     service: "run.googleapis.com",
 });
 
-const enableIamApi = new gcp.projects.Service("EnableIamApi", {
+export const enableIamApi = new gcp.projects.Service("EnableIamApi", {
     service: "iam.googleapis.com",
 });
 
-const enableCloudResourceManagerApi = new gcp.projects.Service("EnableCloudResourceManagerApi", {
+export const enableCloudResourceManagerApi = new gcp.projects.Service("EnableCloudResourceManagerApi", {
     service: "cloudresourcemanager.googleapis.com",
 });
 
-const enableContainerRegistryApi = new gcp.projects.Service("EnableContainerRegistryApi", {
+export const enableContainerRegistryApi = new gcp.projects.Service("EnableContainerRegistryApi", {
     service: "containerregistry.googleapis.com",
 });
--- infra/deploy-iam/index.ts
+++ infra/deploy-iam/index.ts
@@ -1,4 +1,38 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 import * as enableGcpApis from "./enableGcpApis";
-import * as iam from "./iam";
+import * as config from "./config";
+
+// Setup CI service account
+const ciServiceAccount = new gcp.serviceaccount.Account(`ci-svc`, {
+    accountId: `ci-svc`,
+    description: `CI Service Account`,
+    displayName: `CI Service Account`
+}, {dependsOn: enableGcpApis.enableIamApi});
+
+const ciServiceAccountEmail = pulumi.interpolate`serviceAccount:${ciServiceAccount.email}`;
+
+const storageAdminIamBinding = new gcp.projects.IAMBinding(`ci-svc-storage-admin`, {
+    members: [ciServiceAccountEmail],
+    role: "roles/storage.admin"
+}, {parent: ciServiceAccount, dependsOn: enableGcpApis.enableIamApi});
+
+const cloudRunAdminIamBinding = new gcp.projects.IAMBinding(`ci-svc-run-admin`, {
+    members: [ciServiceAccountEmail],
+    role: "roles/run.admin"
+}, {parent: ciServiceAccount, dependsOn: enableGcpApis.enableIamApi});
+
+// Setup cloud run service account
+const appName = config.appName;
+
+const cloudRunServiceAccount = new gcp.serviceaccount.Account(`${appName}-cloud-run`, {
+    accountId: `${appName}-cloud-run`,
+    description: `${appName} cloud run service account`,
+    displayName: `${appName} cloud run`
+}, {dependsOn: enableGcpApis.enableIamApi});
+
+const cloudRunServiceAccountBinding = new gcp.serviceaccount.IAMBinding(`${appName}-cloud-run`, {
+    serviceAccountId: cloudRunServiceAccount.id,
+    members: [ ciServiceAccountEmail ],
+    role: "roles/iam.serviceAccountUser"
+}, {dependsOn: enableGcpApis.enableIamApi});
