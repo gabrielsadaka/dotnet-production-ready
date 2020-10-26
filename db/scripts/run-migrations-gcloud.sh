e9ebb8d06d3daa3784394b486a05f0913f39a9b7
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-26 13:33:11 +1100

    Wait for cloud proxy to start before running migrations

--- db/scripts/run-migrations-gcloud.sh
+++ db/scripts/run-migrations-gcloud.sh
@@ -7,8 +7,16 @@ echo "Connecting to Google Cloud SQL Proxy"
 
 echo $GOOGLE_CREDENTIALS > keyfile.json
 
-./cloud_sql_proxy -credential_file keyfile.json -instances="$GOOGLE_PROJECT:$GOOGLE_REGION:$DB_INSTANCE"=tcp:"$DB_PORT"
+./cloud_sql_proxy -credential_file keyfile.json -instances="$GOOGLE_PROJECT:$GOOGLE_REGION:$DB_INSTANCE"=tcp:"$DB_PORT" &
+
+RETRIES=10
+until PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USERNAME -d $DB_NAME -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for Google Cloud Run Proxy, $((RETRIES--)) remaining attempts..."
+  sleep 1
+done
+
+echo "Google Cloud Run Proxy ready, running migrations"
 
 # migrate the database
->&2 echo "Flyway is running pending migrations -if any- on the database"
+echo "Flyway is running pending migrations -if any- on the database"
 flyway -url=jdbc:postgresql://"$DB_HOST":"$DB_PORT"/"$DB_NAME" -schemas="$DB_SCHEMA_NAME" -user="$DB_USERNAME" -password="$DB_PASSWORD" -locations="$DB_MIGRATIONS_LOCATION" migrate
