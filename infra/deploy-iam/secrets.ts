151a25479c3cf9c79b90066fc052226999d2610f
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-28 14:37:31 +1100

    Move secret management api to where secret is created

--- infra/deploy-iam/secrets.ts
+++ infra/deploy-iam/secrets.ts
@@ -1,11 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 import * as config from "./config";
-import * as enableGcpApis from "./enableGcpApis";
+
+export const enableSecretManagerApi = new gcp.projects.Service("EnableSecretManagerApi", {
+    service: "secretmanager.googleapis.com",
+});
 
 export const databasePasswordSecret = new gcp.secretmanager.Secret(`${config.appName}-db-password`, {
     secretId: `${config.appName}-db-password`,
     replication: {
         automatic: true
     }
-}, {dependsOn: enableGcpApis.enableSecretManagerApi});
+}, {dependsOn: enableSecretManagerApi});
