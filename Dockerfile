aa2e7c3e8808a27469db660360e935c8e039991b
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-28 16:52:03 +1100

    Lint image using dockle before pushing

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,8 @@ RUN dotnet publish -c Release -o out
 
 FROM mcr.microsoft.com/dotnet/core/aspnet:3.1 AS runtime
 COPY --from=build /app/out ./
+
+RUN useradd -d /home/dotnet-user -m -s /bin/bash dotnet-user
+USER dotnet-user
+
 ENTRYPOINT ["dotnet", "WeatherApi.dll"]
--- infra/build-push.sh
+++ infra/build-push.sh
@@ -8,6 +8,10 @@ IMAGE_TAG=gcr.io/"$GOOGLE_PROJECT"/"$APP_NAME":"$GITHUB_SHA"
 
 docker build . -t "$IMAGE_TAG"
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --exit-code 1 --no-progress --severity CRITICAL "$IMAGE_TAG"
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy \
+    --exit-code 1 --no-progress --severity CRITICAL "$IMAGE_TAG"
+
+docker run --rm  --rm -v /var/run/docker.sock:/var/run/docker.sock -i goodwithtech/dockle \
+     --exit-code 1 --exit-level warn "$IMAGE_TAG"
 
 docker push "$IMAGE_TAG"
