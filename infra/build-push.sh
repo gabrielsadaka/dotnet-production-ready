3f8eeab5fa65573f2eea93c1838c4d6be3404d63
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-28 16:31:43 +1100

    Scan image using trivy before pushing

--- infra/build-push.sh
+++ infra/build-push.sh
@@ -4,6 +4,10 @@ set -e
 
 echo $GOOGLE_CREDENTIALS | docker login -u _json_key --password-stdin https://gcr.io
 
-docker build . -t gcr.io/"$GOOGLE_PROJECT"/"$APP_NAME":"$GITHUB_SHA"
+IMAGE_TAG=gcr.io/"$GOOGLE_PROJECT"/"$APP_NAME":"$GITHUB_SHA"
 
-docker push gcr.io/"$GOOGLE_PROJECT"/"$APP_NAME":"$GITHUB_SHA"
+docker build . -t "$IMAGE_TAG"
+
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --exit-code 1 --no-progress --severity CRITICAL "$IMAGE_TAG"
+
+docker push "$IMAGE_TAG"
