a0019ff673268b6cac83b4fa23a74d726752134c
Gabriel Sadaka <gabriel.sadaka@thoughtworks.com>
2020-10-28 14:05:26 +1100

    Update secret creation to depend on secret manager API

--- infra/deploy-iam/secrets.ts
+++ infra/deploy-iam/secrets.ts
@@ -1,10 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 import * as config from "./config";
+import * as enableGcpApis from "./enableGcpApis";
 
 export const databasePasswordSecret = new gcp.secretmanager.Secret(`${config.appName}-db-password`, {
     secretId: `${config.appName}-db-password`,
     replication: {
         automatic: true
     }
-});
+}, {dependsOn: enableGcpApis.enableSecretManagerApi});
